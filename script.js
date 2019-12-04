2797eb0df076f3a1ac89363aebe8bd9840f4e2f5
Turner Jacobs <tsjacobs3@gmail.com>
2019-12-04 18:34:59 +0400

    calulator is fully operational

--- script.js
+++ script.js
@@ -1,41 +1,110 @@
-//Add
+//Global variables and defaults
+var operator;
+
+let display = document.querySelector('#display');
+let digit = document.querySelectorAll('.digit');
+let period = document.querySelector('#period');
+let symbol = document.querySelectorAll('.symbol');
+let clearKey = document.querySelector('#clear');
+let backspace = document.querySelector('#backspace');
+let equalsKey = document.querySelector('#equals');
+
+display.textContent = '';
+
+var values = {
+  a: 0,
+  b: 0,
+};
+
+//Operations
 add = (a, b) => a + b;
-
-//Subtract
 subtract = (a, b) => a - b;
-
-//Multiply
 multiply = (a, b) => a * b;
-
-//Divide
 divide = (a, b) => a / b;
 
-//Operate
+//Operate function
 operate = (a, b, operator) => {
   switch (operator) {
     case 'plus':
-      console.log(add(a, b));
+      result = add(a, b);
       break;
     case 'minus':
-      console.log(subtract(a, b));
+      result = subtract(a, b);
       break;
     case 'multiply':
-      console.log(multiply(a, b));
+      result = multiply(a, b);
       break;
     case 'divide':
-      console.log(divide(a, b));
+      result = divide(a, b);
       break;
   }
-}
+  console.log(result);
+  (result == 'Infinity') ? (display.textContent = 'Error') : 
+    (display.textContent = result.toFixed(2));
+};
 
 //Update display
-const shell = document.querySelector('#shell');
-const display = document.querySelector('#display');
-updateDisplay = () => display.textContent('test');
+updateDisplay = (userInput) => display.textContent = userInput;
 
-let digit = document.querySelectorAll('.number')
-document.querySelectorAll('.number').forEach((numbers) => {
-  button.addEventListener('click', e => {
-    updateDisplay();
+document.querySelectorAll('.digit').forEach((digit) => {
+  digit.addEventListener('click', e => {
+    updateDisplay(display.textContent + digit.textContent);
+    var displayValue = display.textContent;
+    console.log(displayValue);
   });
+});
+
+//Clear Key
+clearKey.addEventListener('click', e => {
+  updateDisplay('');
+  values.a = 0;
+  values.b = 0;
+});
+
+//Operator Keys
+storeValue = (operator) => {
+  console.log(operator);
+  values.a = display.textContent;
+  display.textContent = '';
+}
+
+document.querySelectorAll('.symbol').forEach((symbol) => {
+  symbol.addEventListener('click', e => {
+    if (symbol.id == 'plus') {
+      operator = 'plus';
+    } else if (symbol.id == 'minus') {
+      operator = 'minus'
+    } else if (symbol.id == 'multiply') {
+      operator = 'multiply'
+    } else {
+      operator = 'divide'
+    }
+    storeValue(operator);
+  });
+});
+
+//Equals key
+equalsKey.addEventListener('click', e => {
+  values.b = display.textContent;
+  a = parseFloat(values.a, 10);
+  b = parseFloat(values.b, 10);
+  operate(a, b, operator);
+});
+
+//Period key
+period.addEventListener('click', e => {
+  if ((display.textContent.indexOf('.') > -1) != true) {
+    updateDisplay(display.textContent + period.textContent);
+    var displayValue = display.textContent;
+    console.log(displayValue);
+  }
+});
+
+
+//Backspace key
+backspace.addEventListener('click', e => {
+  let displayString = display.textContent.toString();
+  var displayValue = (displayString.slice(0, (displayString.length -1)));
+  console.log(displayValue);
+  updateDisplay(displayValue);
 });
